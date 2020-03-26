e9580206d4b7edd6186b3e315eb4900e883e2a2b
gezx <1469622853@qq.com>
2020-03-26 23:54:43 +0800

    创建 Java程序学习 study-demos-master项目

---
+++ src/main/java/com/example/studydemosmaster/StudyDemosMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.studydemosmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyDemosMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyDemosMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studydemosmaster/StudyDemosMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studydemosmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDemosMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
